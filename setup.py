a6e84d6e79ed975415db198452352b245dd139c0
ImNotOssy <imnotossy420@gmail.com>
2024-09-03 15:31:45 -0500

    pip install format

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="dspac_investing_API",
     version="0.1.0",
-    packages=["dspac_api"],
+    description="Unofficial dSPAC Invest API written in Python Requests",
+    url="https://github.com/ImNotOssy/dSPAC_investing_API",
+    author="Oswaldo Romero",
+    packages=["dspac_invest_api"],
     install_requires=[
         "pillow",
         "python-dotenv",
